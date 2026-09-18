7081d3e917561296f0c83a8f0a3d90bf2d4514d1
Linruoxin-Sy <342972612@qq.com>
2026-09-18 22:57:12 +0800

    fix: fix the width error of menu

--- app/(infrastructure)/components/AppMenu.tsx
+++ app/(infrastructure)/components/AppMenu.tsx
@@ -6,7 +6,10 @@ const menuItems = [{ label: 'Home', ariaLabel: 'Go to home page', link: '/' }];
 
 export default function AppMenu() {
   return (
-    <div className="fixed top-0 left-0 h-dvh">
+    // 必须给容器一个"确定"的宽度（inset-0 => left/right 都为 0）。
+    // 只写 left-0 时，fixed + width:auto 会 shrink-to-fit，而内部全是 absolute 子元素，
+    // 于是容器宽度算成 0；StaggeredMenu 在 ≤1024px 用 width:100% 的面板就会塌成 0 宽。
+    <div className="fixed top-0 left-0 h-dvh w-full md:w-80 lg:w-120">
       <StaggeredMenu
         position="left"
         items={menuItems}
